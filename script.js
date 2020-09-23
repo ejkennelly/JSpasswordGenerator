d00b47f63986960f8cc2203435c4a92b65e3f522
Eliza Kennelly <elizakennelly@gmail.com>
2020-09-23 18:33:57 -0400

    Created functions to generate random characters

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ function writePassword() {
 
 }
 
+// Functions to generate random characters
+function getRandomUppercase() {
+    return String.fromCharCode(Math.floor(Math.random()*26 + 65);
+}
+function getRandomLowercase() {
+    return String.fromCharCode(Math.floor(Math.random()*26 + 97))
+}
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random()*10 + 48))
+}
+function getRandomSymbol() {
+    var symbol = ["!@#$%^&*(){}[]=<>/,.|~?"];
+    return symbol[Math.floor(Math.random)()*symbol.length)];
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+prompt("How long do you want your password to be?");
