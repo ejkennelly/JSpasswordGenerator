6459c6f25aa445abdee2bd9f2dba1c9f6107329c
Eliza Kennelly <elizakennelly@gmail.com>
2020-09-24 22:46:33 -0400

    Final working code

--- script.js
+++ script.js
@@ -10,64 +10,71 @@ function writePassword() {
 
 }
 
-// // VARIABLES
-// //===============================================
-var userChoices= { 
-    wantUpper: function() {
-    confirm ("Do you want to use uppercase characters in your password?");
-    },
-    wantLower: function() {
-        confirm ("Do you want to use lowercase characters in your password?");
-    },
-    wantNumeric: function() {
-         confirm ("Do you want to use numeric characters in your password?");
-    },
-    wantSpecial: function() {
-        confirm ("Do you want to use special characters in your password?");
+function generatePassword() {
+    //Beginning prompt requiring a number between 8 and 128 to run rest of code
+
+    //While isValid is false, run password length requirements. Once isValid is true, move onto next block
+    var isValid = false;
+    do {
+        var passLength = prompt("How long do you want your password to be?");
+        passLength = parseInt(passLength);
+
+        if ((passLength < 8) || (passLength > 128) || (isNaN(passLength)))
+            alert("Must be a number between 8 and 128!");
+        else
+            isValid = true;
+
+    } while (!isValid);
+
+    //While isValid is false, run password options. Once true, move on
+    isValid = false;
+    do {
+        // Password options/requirements
+        var wantUpper = confirm("Do you want to use uppercase characters in your password?");
+        var wantLower = confirm("Do you want to use lowercase characters in your password?");
+        var wantNumeric = confirm("Do you want to use numeric characters in your password?");
+        var wantSpecial = confirm("Do you want to use special characters in your password?");
+        var result = [];
+
+        //If no conditions are confirmed
+        if ((wantUpper == false) && (wantLower == false) && (wantNumeric == false) && (wantSpecial == false))
+            alert("You have to select at least one condition.");
+        else
+            isValid = true;
+    } while (!isValid);
+
+
+    // possible password value arrays
+    var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", ",", ".", "|", "~", "?"];
+    var returnVar = "";
+
+    if ((passLength >= 8) && (passLength <= 128) && ((wantUpper == true) || (wantLower == true) || (wantNumeric == true) || (wantSpecial === true))) {
+        if (wantUpper == true) {
+            for (var i of upper)
+                result.push(i);
+        }
+        if (wantLower == true) {
+            for (var i of upper)
+                //Makes upper array become lowercase letters
+                result.push(i.toLowerCase(upper));
+        }
+
+        if (wantNumeric == true) {
+            for (var i of numeric)
+                result.push(i);
+        }
+        if (wantSpecial == true) {
+            for (var i of specialChar)
+                result.push(i);
+        }
+        for (var i = 0; i < passLength; i++)
+            returnVar += result[Math.floor(Math.random() * result.length)];
     }
-}
 
-
-
-// // FUNCTIONS
-// //================================================
-// function logUserChoices() {
-//     console.log(userChoices.wantUpper);
-//     console.log(userChoices.wantLower);
-//     console.log(userChoices.wantNumeric);
-//     console.log(userChoices.wantSpecial);
-// }
-function getRandomUppercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    return returnVar;
 }
-function getRandomLowercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-}
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-}
-function getRandomSymbol() {
-    var symbol = ["!@#$%^&*(){}[]=<>/,.|~?"];
-    return symbol(Math.floor(Math.random)() * symbol.length);
-}
-function passCharCount(x) {
-    if (x < 8 || x > 128) {
-        alert("You must pick a number between 8 and 128");
-    }
-    else (x >= 8 || x <= 128);
-    console.log(document.getElementById("password"));
-}
-
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
-
-// MAIN PROCESS
-//===========================================
-passwordText= prompt("How long do you want your password to be?");{
-    passCharCount(passwordText);{
-    console.log(passwordText);
-    }
-}
-
-//If wantUpper run get randomUppercase function
-//If wantLower run get randomLowercase function
